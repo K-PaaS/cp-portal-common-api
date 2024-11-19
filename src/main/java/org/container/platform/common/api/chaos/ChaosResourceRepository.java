58fe97c18a5de2de0336ba22d2a5f83673fcae80
Luna-irevit <lsy77877korea@gmail.com>
2024-11-19 16:39:05 +0900

    Add chaos getResourceUsageByHpaPod

--- src/main/java/org/container/platform/common/api/chaos/ChaosResourceRepository.java
+++ src/main/java/org/container/platform/common/api/chaos/ChaosResourceRepository.java
@@ -21,16 +21,17 @@ import java.util.List;
 @Transactional
     public interface ChaosResourceRepository extends JpaRepository<ChaosResource, Long>, JpaSpecificationExecutor<ChaosResource>  {
     @Query(value = "SELECT * FROM cp_chaos_resource WHERE chaos_id = :chaosId ;", nativeQuery = true)
-    List<ChaosResource> findByChaosId(@Param("chaosId") Long chaosId);
+    List<ChaosResource> findAllByChaosId(@Param("chaosId") Long chaosId);
 
     @Query(value = "SELECT * FROM cp_chaos_resource WHERE chaos_id = :chaosId AND choice = 1 ;", nativeQuery = true)
     List<ChaosResource> findAllByChoice(@Param("chaosId") Long chaosId);
+
 
     @Query(value = "SELECT generate_name FROM cp_chaos_resource WHERE chaos_id = :chaosId AND type = 'pod' GROUP BY generate_name ;", nativeQuery = true)
     List<String> findGenerateNameByChaosId(@Param("chaosId") Long chaosId);
 
     @Query(value = "SELECT * FROM cp_chaos_resource WHERE chaos_id = :chaosId AND type = :type ;", nativeQuery = true)
-    List<ChaosResource> findAllByChaosId(@Param("chaosId") Long chaosId, @Param("type") String type);
+    List<ChaosResource> findAllByChaosIdAndType(@Param("chaosId") Long chaosId, @Param("type") String type);
 
     @Query("SELECT COUNT(c) > 0 FROM ChaosResource c WHERE c.stressChaos.chaosId = :chaosId AND c.resourceName = :resourceName")
     boolean existsByChaosIdAndResourceName(@Param("chaosId") Long chaosId, @Param("resourceName") String resourceName);
--- src/main/java/org/container/platform/common/api/chaos/ChaosService.java
+++ src/main/java/org/container/platform/common/api/chaos/ChaosService.java
@@ -1,6 +1,5 @@
 package org.container.platform.common.api.chaos;
 
-import org.container.platform.common.api.clusters.Clusters;
 import org.container.platform.common.api.common.CommonService;
 import org.container.platform.common.api.common.Constants;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -112,7 +111,7 @@ public class ChaosService {
      * @return the ChaosResource info list
      */
     public ChaosResourceList getChaosResourceList(Long chaosId) {
-        ChaosResourceList chaosResourcesList = new ChaosResourceList(chaosResourceRepository.findByChaosId(chaosId));
+        ChaosResourceList chaosResourcesList = new ChaosResourceList(chaosResourceRepository.findAllByChaosId(chaosId));
         return (ChaosResourceList) commonService.setResultModel(chaosResourcesList, Constants.RESULT_STATUS_SUCCESS);
     }
 
@@ -137,6 +136,78 @@ public class ChaosService {
     public ResourceUsage getResourceUsageByPod(String chaosName) {
         Long chaosId = stressChaosRepository.findByName(chaosName);
         List<ChaosResource> chaosResourceList = chaosResourceRepository.findAllByChoice(chaosId);
+        ResourceUsage  resourceUsage = new ResourceUsage();
+        ResourceUsageItem resourceUsageItem = new ResourceUsageItem();
+        int count = 0;
+
+        for(ChaosResource chaosResource : chaosResourceList ){
+            resourceUsageItem.getResourceName().add(chaosResource.getResourceName());
+            List<ChaosResourceUsage>  chaosResourceUsageList = chaosResourceUsageRepository.findAllByResourceId(chaosResource.getResourceId());
+
+            List<Integer> cpu = new ArrayList<>();
+            List<Integer> memory = new ArrayList<>();
+            List<Integer> appStatus = new ArrayList<>();
+
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
+            LocalDateTime firstDateTime = LocalDateTime.parse(chaosResourceUsageList.get(0).getChaosResourceUsageId().getMeasurementTime(), formatter);
+
+            if(chaosResourceUsageList.size() == 12) { // 모든 측정 시간이 있는 경우
+                for(ChaosResourceUsage chaosResourceUsage : chaosResourceUsageList){
+                    cpu.add(Math.toIntExact(chaosResourceUsage.getCpu()));
+                    memory.add(Math.toIntExact(chaosResourceUsage.getMemory()));
+                    appStatus.add(chaosResourceUsage.getAppStatus());
+                    if(count == 0){
+                        resourceUsageItem.getTime().add(chaosResourceUsage.getChaosResourceUsageId().getMeasurementTime());
+                    }
+                }
+            }else {// 모든 측정 시간이 없는 경우
+                if(count == 0) {
+                    for (int i = 0; i < 12; i++) {
+                        LocalDateTime measurementTime = firstDateTime.plusSeconds(i * 10);
+                        resourceUsageItem.getTime().add(measurementTime.format(formatter));
+                    }
+                }
+
+                for(String measurementTime : resourceUsageItem.getTime()) {
+                    boolean check = false;
+                    for (ChaosResourceUsage chaosResourceUsage : chaosResourceUsageList) {
+                        if (chaosResourceUsage.getChaosResourceUsageId().getMeasurementTime().equals(measurementTime)) {
+                            cpu.add(Math.toIntExact(chaosResourceUsage.getCpu()));
+                            memory.add(Math.toIntExact(chaosResourceUsage.getMemory()));
+                            appStatus.add(chaosResourceUsage.getAppStatus());
+                            check = true;
+                            break;
+                        }
+                    }
+
+                    if (!check) {
+                        cpu.add(-1);
+                        memory.add(-1);
+                        appStatus.add(-1);
+                    }
+                }
+            }
+
+            count++;
+            resourceUsageItem.getCpu().add(cpu);
+            resourceUsageItem.getMemory().add(memory);
+            resourceUsageItem.getAppStatus().add(appStatus);
+        }
+        resourceUsage.addItem(resourceUsageItem);
+        return (ResourceUsage) commonService.setResultModel(resourceUsage, Constants.RESULT_STATUS_SUCCESS);
+    }
+
+    /**
+     *  Resource usage by Pods during chaos 조회(Get Resource Usage by Pods during chaos)
+     */
+    public ResourceUsage getResourceUsageByHpaPod(String chaosName) {
+        Long chaosId = stressChaosRepository.findByName(chaosName);
+        List<ChaosResource> chaosResourceList = chaosResourceRepository.findAllByChaosIdAndType(chaosId, "pod");
+
+        if (chaosResourceList.size() > 6) {
+            chaosResourceList.subList(6, chaosResourceList.size()).clear();
+        }
+
         ResourceUsage  resourceUsage = new ResourceUsage();
         ResourceUsageItem resourceUsageItem = new ResourceUsageItem();
         int count = 0;
@@ -268,7 +339,7 @@ public class ChaosService {
      */
     public ResourceUsage getResourceUsageByNode(String chaosName) {
         Long chaosId = stressChaosRepository.findByName(chaosName);
-        List<ChaosResource> chaosResourceList = chaosResourceRepository.findAllByChaosId(chaosId, "node");
+        List<ChaosResource> chaosResourceList = chaosResourceRepository.findAllByChaosIdAndType(chaosId, "node");
         ResourceUsage  resourceUsage = new ResourceUsage();
         ResourceUsageItem resourceUsageItem = new ResourceUsageItem();
         int count = 0;
